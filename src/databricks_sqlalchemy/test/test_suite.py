53e63ab4a268513aeebbf5f3df959bd9313b4722
Jothi Prakash <jothi.prakash@databricks.com>
2024-08-19 12:14:21 +0530

    databricks-sqlalchemy plugin split

---
+++ src/databricks_sqlalchemy/test/test_suite.py
@@ -0,0 +1,12 @@
+"""
+The order of these imports is important. Test cases are imported first from SQLAlchemy,
+then are overridden by our local skip markers in _regression, _unsupported, and _future.
+"""
+
+
+# type: ignore
+# fmt: off
+from sqlalchemy.testing.suite import *
+from databricks_sqlalchemy.src.sqlalchemy.test._regression import *
+from databricks.sqlalchemy.test._unsupported import *
+from databricks.sqlalchemy.test._future import *
