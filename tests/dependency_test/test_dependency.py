86d92d4e0cbb84f0a83b882d094124fb511ff0a0
Jothi Prakash <jothi.prakash@databricks.com>
2025-01-28 05:28:48 +0530

    Relaxed the support for python and connector version (#14)
    * Relaxed the connector to be greater than 4.0.0
    
    * fixed actions

--- tests/dependency_test/test_dependency.py
+++ tests/dependency_test/test_dependency.py
@@ -1,21 +1,34 @@
 import pytest
 
+
 class DatabricksImportError(Exception):
     pass
 
-class TestLibraryDependencySuite:
 
-    @pytest.mark.skipif(pytest.importorskip("databricks_sql_connector"), reason="databricks_sql_connector is present")
+class TestLibraryDependencySuite:
+    @pytest.mark.skipif(
+        pytest.importorskip("databricks_sql_connector"),
+        reason="databricks_sql_connector is present",
+    )
     def test_sql_core(self):
-        with pytest.raises(DatabricksImportError, match="databricks_sql_connector module is not available"):
+        with pytest.raises(
+            DatabricksImportError,
+            match="databricks_sql_connector module is not available",
+        ):
             try:
                 import databricks
             except ImportError:
-                raise DatabricksImportError("databricks_sql_connector_core module is not available")
+                raise DatabricksImportError(
+                    "databricks_sql_connector_core module is not available"
+                )
 
-    @pytest.mark.skipif(pytest.importorskip("sqlalchemy"), reason="SQLAlchemy is present")
+    @pytest.mark.skipif(
+        pytest.importorskip("sqlalchemy"), reason="SQLAlchemy is present"
+    )
     def test_sqlalchemy(self):
-        with pytest.raises(DatabricksImportError, match="sqlalchemy module is not available"):
+        with pytest.raises(
+            DatabricksImportError, match="sqlalchemy module is not available"
+        ):
             try:
                 import sqlalchemy
             except ImportError:
